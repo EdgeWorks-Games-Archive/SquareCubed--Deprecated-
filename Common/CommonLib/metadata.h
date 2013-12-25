37f6758d0898ac23b1173dc156c8c32e36189749
Layl Conway <laylconway@live.com>
2013-12-25 02:55:55 +0100

    gdefines.h now correctly gets version from CMake generated files.

--- Common/CommonLib/metadata.h
+++ Common/CommonLib/metadata.h
@@ -1,3 +1,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 3
 #define VERSION_PATCH 0
+
+#define _MTS(x) #x
+/** Converts a macro to string literal. */
+#define MTS(x) _MTS(x)
--- Server/WorldServerBase/gdefines.h
+++ Server/WorldServerBase/gdefines.h
@@ -1,17 +1,27 @@
 #pragma once
+
+#include <CommonLib/metadata.h>
+
 // This file is for extra gloabl defines, think before you add something because you probably don't need to.
 
+/** Used instead of _DEBUG because a pre-release test could be built with release target. */
 //#define _FULL_RELEASE
 
+#if VERSION_PATCH == 0
+#define RAW_APP_VERSION MTS(VERSION_MAJOR)"."MTS(VERSION_MINOR)
+#else
+#define RAW_APP_VERSION MTS(VERSION_MAJOR)"."MTS(VERSION_MINOR)"."MTS(VERSION_PATCH)
+#endif
+
 // Application Information
-#ifdef _FULL_RELEASE
+#ifndef _FULL_RELEASE
 #define APP_NAME "SquareCubed World Server"
-#define APP_VERSION "0.3"
+#define APP_VERSION RAW_APP_VERSION
 #define APP_AUTHOR "EdgeWorks Games™"
 #define STR_SUPPORT "Supported release version!"
 #else
 #define APP_NAME "SquareCubed World Server"
-#define APP_VERSION "0.3-dev"
+#define APP_VERSION RAW_APP_VERSION"-dev"
 #define APP_AUTHOR "EdgeWorks Games™"
 #define STR_SUPPORT "Unsupported development version!"
 #endif
