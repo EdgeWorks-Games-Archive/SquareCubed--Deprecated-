1b0500b3f15bbfd59dec45036ee4f412cd0eb312
KodenC <kodencmu@gmail.com>
2014-01-01 15:06:11 +0100

    Added multi-selection using shift. Fixed an issue where the same unit could be selected several times.

--- Client/ClientBase/UnitSelect.cpp
+++ Client/ClientBase/UnitSelect.cpp
@@ -28,20 +28,42 @@ namespace Tools {
 				const std::list<std::unique_ptr<IUnit>>& unitList = m_Units.GetAll();
 				bool noHit = true;
 
+				// Something already selected, not holding shift for multi-selection
+				if (!m_Input.GetKeyMods().Shift && !m_SelectedUnits.empty())
+					m_SelectedUnits.clear();
+
 				for (const std::unique_ptr<IUnit> &unit : unitList) {
+					// TODO: Needs common rigidbody interface but for now this works
 					auto dUnit = static_cast<const DynamicUnit*>(unit.get());
 					if (dUnit->RigidBody.BroadphaseAABB.Contains(m_Input.GetCursorPosition().World)) {
-						m_SelectedUnits.push_back(*unit);
-						noHit = false;
+						// If it's already in the list then selecting it twice is kindof pointless
+						bool inList = false;
+						auto it = m_SelectedUnits.begin();
+						while (it != m_SelectedUnits.end()) {
+							if (it->get().ID == unit->ID) {
+								// Found it!
+								inList = true;
+								noHit = false;
+								break;
+							}
+							else
+								++it;
+						}
+						// However - if it isn't, we do want to select it.
+						if (!inList) {
+							m_SelectedUnits.push_back(*unit);
+							noHit = false;
+						}
 					}
 				}
-
+				// If no unit was hit at all during the click, that is a call for a selection clear.
 				if (noHit)
 					m_SelectedUnits.clear();
 			}
-			
-			if (args.MouseButton == Input::MouseButton::Right && args.IsPressed)
+
+			if (args.MouseButton == Input::MouseButton::Right && args.IsPressed) {
 				printf("wow you pressed the right mouse button well done\n");
+			}
 		}
 	}
 }
