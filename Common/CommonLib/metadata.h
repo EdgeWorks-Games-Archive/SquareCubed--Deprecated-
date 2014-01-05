6a1b021169bb2954f594804b51039430c92d7238
Layl Conway <laylconway@live.com>
2014-01-05 20:24:04 +0100

    Added temp control cmake variable.

--- Common/CommonLib/metadata.h
+++ Common/CommonLib/metadata.h
@@ -2,6 +2,9 @@
 #define VERSION_MINOR 3
 #define VERSION_PATCH 0
 
+// TEMP SETTINGS
+#define GAME_TEMP_CONTROLS_SETTING 0
+
 #define _MTS(x) #x
 /** Converts a macro to string literal. */
 #define MTS(x) _MTS(x)
